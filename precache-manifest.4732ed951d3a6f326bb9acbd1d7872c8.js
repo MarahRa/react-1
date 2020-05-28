76f12b53b14ee396bd56cb12945cfa91eed662c3
Marah <Marah_rayan@yahoo.com>
2020-05-28 12:53:26 +0100

    Updates

---
+++ precache-manifest.4732ed951d3a6f326bb9acbd1d7872c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a446f7c4fb9fe3881a52d96fdf96167",
+    "url": "/react-1/index.html"
+  },
+  {
+    "revision": "cebcde75b606f83891a0",
+    "url": "/react-1/static/css/main.869040ec.chunk.css"
+  },
+  {
+    "revision": "6e82b7b3d644147523b3",
+    "url": "/react-1/static/js/2.ad121fa8.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/react-1/static/js/2.ad121fa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cebcde75b606f83891a0",
+    "url": "/react-1/static/js/main.10c0a5bb.chunk.js"
+  },
+  {
+    "revision": "91375caf370672eeecff",
+    "url": "/react-1/static/js/runtime-main.7756cdd4.js"
+  }
+]);
